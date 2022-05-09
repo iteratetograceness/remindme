82d82efde61e0e25553720395b1bc61e176c569a
Grace Yun <74513600+jueungrace@users.noreply.github.com>
2022-05-09 13:23:18 -0400

    testing

--- app.js
+++ app.js
@@ -15,8 +15,43 @@ const app = new App({
       storeInstallation: async (installation) => {
           console.log(installation)
       }
-  }
+  },
 });
+
+app.event('app_home_opened', async ({ event, client, logger }) => {
+    try {
+      // Call views.publish with the built-in client
+      const result = await client.views.publish({
+        // Use the user ID associated with the event
+        user_id: event.user,
+        view: {
+          // Home tabs must be enabled in your app configuration page under "App Home"
+          "type": "home",
+          "blocks": [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome home, <@" + event.user + "> :house:*"
+              }
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+              }
+            }
+          ]
+        }
+      });
+  
+      logger.info(result);
+    }
+    catch (error) {
+      logger.error(error);
+    }
+  });
 
 const startApp = async () => {
 
