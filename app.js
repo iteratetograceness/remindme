228f8e2bb139590338f648cbeb25503aa7af1769
Jueun Grace Yun <graceyunn@gmail.com>
2022-05-26 19:31:07 -0400

    modal approach

--- app.js
+++ app.js
@@ -1,137 +1,293 @@
-const { App, LogLevel } = require('@slack/bolt');
-const NodeCache = require( "node-cache" );
-const { v4: uuid } = require('uuid');
+const { App, LogLevel } = require("@slack/bolt");
+const NodeCache = require("node-cache");
+const { DateTime } = require("luxon");
+const { v4: uuid } = require("uuid");
+var memwatch = require("@airbnb/node-memwatch");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = 5000;
 
 const cache = new NodeCache();
 
 const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  clientId: process.env.SLACK_CLIENT_ID,
-  clientSecret: process.env.SLACK_CLIENT_SECRET,
-  stateSecret: 'remind-me-secret',
-  scopes: ['chat:write','commands','chat:write.public'],
-  port: PORT,
-  logLevel: LogLevel.DEBUG,
-  customRoutes: [
-    {
-      path: '/',
-      method: ['GET'],
-      handler: (_, res) => {
-        res.writeHead(200);
-        res.end('Welcome to the homepage of the remindme Slack bot.');
-      },
-    },
-    {
-        path: '/support',
-        method: ['GET'],
-        handler: (_, res) => {
-          res.writeHead(200);
-          res.end('For support, please email graceyunn@gmail.com.');
-        },
-      },
-      {
-        path: '/privacy',
-        method: ['GET'],
-        handler: (_, res) => {
-          res.writeHead(200);
-          res.end(`This bot requests the following scopes: 'chat:write','commands','chat:write.public'.`);
-        },
-      },
-  ],
-  installationStore: {
-      storeInstallation: async (installation) => {
-        if (installation.isEnterpriseInstall && installation.enterprise !== undefined) { 
-            cache.set(installation.enterprise.id, JSON.stringify(installation))
-            process.env[installation.enterprise.id] =  JSON.stringify(installation)
-            return
-        }
-        if (installation.team !== undefined) { 
-            cache.set(installation.team.id, JSON.stringify(installation))
-            process.env[installation.team.id] =  JSON.stringify(installation)
-            return
-        }
-        throw new Error('Failed saving installation data to installationStore');
-      },
-      fetchInstallation: async (installQuery) => {
-        if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-            return JSON.parse(cache.get(installQuery.enterpriseId)) || JSON.parse(process.env[installQuery.enterpriseId]);
-        }
-        if (installQuery.teamId !== undefined) {
-            return JSON.parse(cache.get(installQuery.teamId)) || JSON.parse(process.env[installQuery.teamId]);
-        }
-        throw new Error('Failed fetching installation');
-      },
-      deleteInstallation: async (installQuery) => {
-        if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-            process.env[installQuery.enterpriseId] = null
-            cache.del(installQuery.enterpriseId)
-            return
-        }
-        if (installQuery.teamId !== undefined) {
-            process.env[installQuery.teamId] = null
-            cache.del(installQuery.teamId)
-        }
-        throw new Error('Failed to delete installation');
-      }
-  },
-  installerOptions: {
-      redirectUriPath: '/slack/redirect',
-  }
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+	clientId: process.env.SLACK_CLIENT_ID,
+	clientSecret: process.env.SLACK_CLIENT_SECRET,
+	stateSecret: "remind-me-secret",
+	scopes: ["chat:write", "commands", "chat:write.public"],
+	port: PORT,
+	logLevel: LogLevel.DEBUG,
+	customRoutes: [
+		{
+			path: "/",
+			method: ["GET"],
+			handler: (_, res) => {
+				res.writeHead(200);
+				res.end("Welcome to the homepage of the remindme Slack bot.");
+			},
+		},
+		{
+			path: "/support",
+			method: ["GET"],
+			handler: (_, res) => {
+				res.writeHead(200);
+				res.end("For support, please email graceyunn@gmail.com.");
+			},
+		},
+		{
+			path: "/privacy",
+			method: ["GET"],
+			handler: (_, res) => {
+				res.writeHead(200);
+				res.end(
+					`This bot requests the following scopes: 'chat:write','commands','chat:write.public'.`
+				);
+			},
+		},
+	],
+	installationStore: {
+		storeInstallation: async (installation) => {
+			if (
+				installation.isEnterpriseInstall &&
+				installation.enterprise !== undefined
+			) {
+				cache.set(installation.enterprise.id, JSON.stringify(installation));
+				return;
+			}
+			if (installation.team !== undefined) {
+				cache.set(installation.team.id, JSON.stringify(installation));
+				return;
+			}
+			throw new Error("Failed saving installation data to installationStore");
+		},
+		fetchInstallation: async (installQuery) => {
+			if (
+				installQuery.isEnterpriseInstall &&
+				installQuery.enterpriseId !== undefined
+			) {
+				return JSON.parse(cache.get(installQuery.enterpriseId));
+			}
+			if (installQuery.teamId !== undefined) {
+				return JSON.parse(cache.get(installQuery.teamId));
+			}
+			throw new Error("Failed fetching installation");
+		},
+		deleteInstallation: async (installQuery) => {
+			if (
+				installQuery.isEnterpriseInstall &&
+				installQuery.enterpriseId !== undefined
+			) {
+				process.env[installQuery.enterpriseId] = null;
+				cache.del(installQuery.enterpriseId);
+				return;
+			}
+			if (installQuery.teamId !== undefined) {
+				process.env[installQuery.teamId] = null;
+				cache.del(installQuery.teamId);
+			}
+			throw new Error("Failed to delete installation");
+		},
+	},
+	installerOptions: {
+		redirectUriPath: "/slack/redirect",
+	},
 });
 
 /**
  * Schedule message to send to certain channel or user from start to end dates
  * Parameters: [@userid/#channel] [start: mm/dd/yyyy] [end: mm/dd/yyyy] [time: hh:mm(am/pm)] [message]
  */
-app.command('/reminders', async ({ payload, context, respond, ack }) => {
-    await ack();
+app.command("/schedule", async ({ ack, body, logger }) => {
+	await ack();
 
-    const { text } = payload;
-    const splitArr = text.split(' ');
-    const id = splitArr[0];
-    const start = splitArr[1];
-    const end = splitArr[2];
-    const time = splitArr[3]
-    const message = splitArr.slice(4).join(' ');
+	try {
+		const result = await app.client.views.open({
+			trigger_id: body,
+			view: {
+				type: "modal",
+				callback_id: "schedule",
+				title: {
+					type: "plain_text",
+					text: "Remind Me",
+					emoji: true,
+				},
+				submit: {
+					type: "plain_text",
+					text: "Submit",
+					emoji: true,
+				},
+				close: {
+					type: "plain_text",
+					text: "Cancel",
+					emoji: true,
+				},
+				blocks: [
+					{
+						type: "context",
+						elements: [
+							{
+								type: "plain_text",
+								text: "Author: K A Applegate",
+								emoji: true,
+							},
+						],
+					},
+					{
+						type: "input",
+						element: {
+							type: "plain_text_input",
+							action_id: "ml_input",
+							multiline: true,
+							placeholder: {
+								type: "plain_text",
+								text: "What are you trying to send?",
+							},
+						},
+						label: {
+							type: "plain_text",
+							text: "Message",
+						},
+						hint: {
+							type: "plain_text",
+							text: "You should probably keep it short. Don't be a paragraph person.",
+						},
+					},
+					{
+						type: "section",
+						text: {
+							type: "mrkdwn",
+							text: "Select a recipient:",
+						},
+						accessory: {
+							type: "users_select",
+							placeholder: {
+								type: "plain_text",
+								text: "Select a user",
+								emoji: true,
+							},
+							action_id: "users_select-action",
+						},
+					},
+					{
+						type: "input",
+						element: {
+							type: "timepicker",
+							initial_time: "13:37",
+							placeholder: {
+								type: "plain_text",
+								text: "Select time",
+								emoji: true,
+							},
+							action_id: "timepicker-action",
+						},
+						label: {
+							type: "plain_text",
+							text: "Time",
+							emoji: true,
+						},
+					},
+					{
+						type: "input",
+						element: {
+							type: "datepicker",
+							initial_date: "2022-04-28",
+							placeholder: {
+								type: "plain_text",
+								text: "Select a start date",
+								emoji: true,
+							},
+							action_id: "datepicker-action",
+						},
+						label: {
+							type: "plain_text",
+							text: "Start Date",
+							emoji: true,
+						},
+					},
+					{
+						type: "input",
+						element: {
+							type: "datepicker",
+							initial_date: "2022-05-27",
+							placeholder: {
+								type: "plain_text",
+								text: "Select an end date",
+								emoji: true,
+							},
+							action_id: "datepicker-action",
+						},
+						label: {
+							type: "plain_text",
+							text: "End Date",
+							emoji: true,
+						},
+					},
+				],
+			},
+		});
+		logger.info(result);
+	} catch (error) {
+		logger.error(error);
+	}
 
-    // Get user or channel id
-    const sanitizedId = id.split('|')[0].match(/[a-zA-Z0-9]+/g).toString();
+	// const { text } = payload;
+	// const splitArr = text.split(" ");
+	// const id = splitArr[0];
+	// const start = splitArr[1];
+	// const end = splitArr[2];
+	// const time = splitArr[3];
+	// const message = splitArr.slice(4).join(" ");
 
-    // Get hours and minutes
-    const splitTime = time.split(':')
-    const amOrPm = splitTime[splitTime.length-1].match(/[a-zA-Z]+/g)[0].toLowerCase();
-    const hours = (splitTime.length === 2 ? Number(splitTime[0]) : Number(splitTime[0].match(/\d+/g))) + (amOrPm === 'pm' ? 12 : 0);
-    const mins = splitTime.length === 2 ? Number(splitTime[1].match(/\d+/g)) : 0;
+	// // Get user or channel id
+	// const sanitizedId = id
+	// 	.split("|")[0]
+	// 	.match(/[a-zA-Z0-9]+/g)
+	// 	.toString();
 
-    // Generate dates
-    const dates = generateDates(start, end, hours, mins);
+	// // Get hours and minutes
+	// const splitTime = time.split(":");
+	// const amOrPm = splitTime[splitTime.length - 1]
+	// 	.match(/[a-zA-Z]+/g)[0]
+	// 	.toLowerCase();
+	// const hours =
+	// 	(splitTime.length === 2
+	// 		? Number(splitTime[0])
+	// 		: Number(splitTime[0].match(/\d+/g))) + (amOrPm === "pm" ? 12 : 0);
+	// const mins = splitTime.length === 2 ? Number(splitTime[1].match(/\d+/g)) : 0;
 
-    console.log(dates);
+	// // Generate dates
+	// const dates = generateDates(start, end, hours, mins);
 
-    // Schedule messages and save reference in cache
-    const messageIds = await scheduleMessages(sanitizedId, message, dates, context.botToken);
-    const referenceId = uuid();
-    const TTL = ((new Date(end) + 1) - new Date()) / 1000;
-    cache.set(referenceId, messageIds, TTL); 
+	// console.log(dates);
 
-    // Respond with reference id
-    await respond(`Niiiiiiiiiiice, successfully scheduled your message: ${message}. This is your reference ID: ${referenceId}. You'll need it if you ever want to cancel your scheduled messages.`);
+	// // Schedule messages and save reference in cache
+	// const messageIds = await scheduleMessages(
+	// 	sanitizedId,
+	// 	message,
+	// 	dates,
+	// 	context.botToken
+	// );
+	// const referenceId = uuid();
+	// const TTL = (new Date(end) + 1 - new Date()) / 1000;
+	// cache.set(referenceId, messageIds, TTL);
+});
+
+app.view("schedule", async ({ ack, body, view, client, logger }) => {
+	await ack();
+	console.log(view);
 });
 
 /**
  * Cancel scheduled messages
  * Parameters: referenceId
  */
-app.command('/cancel', async ({ payload, context, ack, respond }) => {
-    await ack();
-    const { text } = payload;
-    const messageIds = cache.get(text.trim())
-    await deleteScheduledMessages(messageIds, context.botToken);
-    await respond('Messages unscheduled.');
+app.command("/cancel", async ({ payload, context, ack, respond }) => {
+	await ack();
+	const { text } = payload;
+	const messageIds = cache.get(text.trim());
+	await deleteScheduledMessages(messageIds, context.botToken);
+	await respond("Messages unscheduled.");
 });
 
 /**
@@ -139,61 +295,69 @@ app.command('/cancel', async ({ payload, context, ack, respond }) => {
  * @param {string} lastDay - Date String
  */
 const generateDates = (start, end, hours, minutes) => {
-    const dates = [];
-    const date = new Date(start);
+	const dates = [];
+	const date = new Date(start);
 
-    const firstDate = date.setHours(hours, minutes, 0)
-    dates.push(Math.floor(new Date(firstDate).getTime() / 1000))
+	const firstDate = date.setHours(hours, minutes, 0);
+	dates.push(Math.floor(new Date(firstDate).getTime() / 1000));
 
-    let dateString = '';
-    const endDate = new Date(end);
-    endDate.setHours(hours, minutes, 0);
-    const endDateString = endDate.toUTCString();
+	let dateString = "";
+	const endDate = new Date(end);
+	endDate.setHours(hours, minutes, 0);
+	const endDateString = endDate.toUTCString();
 
-    while (dateString !== endDateString) {
-        date.setDate(date.getDate() + 1);
-        date.setHours(hours, minutes, 0); 
-        dateString = date.toUTCString();
-        dates.push(Math.floor(new Date(date).getTime() / 1000));
-    }
-    
-    return dates;
-}
+	while (dateString !== endDateString) {
+		date.setDate(date.getDate() + 1);
+		date.setHours(hours, minutes, 0);
+		dateString = date.toUTCString();
+		dates.push(Math.floor(new Date(date).getTime() / 1000));
+	}
+
+	return dates;
+};
 
 const scheduleMessages = async (id, message, dateArray, token) => {
-    const messageIds = []
-    for (let date of dateArray) {
-        try {
-            const response = await app.client.chat.scheduleMessage({
-                channel: id,
-                text: message,
-                post_at: date,
-                token
-            });
-            console.log('> Scheduled messaged: ', date, response)
-            messageIds.push([response.scheduled_message_id, id])
-        } catch (error) {
-            console.error('> Ran into error scheduling message for', date, JSON.stringify(error));
-        }
-    };
-    return messageIds;
-}
+	const messageIds = [];
+	for (let date of dateArray) {
+		try {
+			const response = await app.client.chat.scheduleMessage({
+				channel: id,
+				text: message,
+				post_at: date,
+				token,
+			});
+			console.log("> Scheduled messaged: ", date, response);
+			messageIds.push([response.scheduled_message_id, id]);
+		} catch (error) {
+			console.error(
+				"> Ran into error scheduling message for",
+				date,
+				JSON.stringify(error)
+			);
+		}
+	}
+	return messageIds;
+};
 
 const deleteScheduledMessages = async (messageArray, token) => {
-    for (let message of messageArray) {
-        try {
-            await app.client.chat.deleteScheduledMessage({
-                channel: message[1],
-                scheduled_message_id: message[0],
-                token,
-            });
-        } catch (error) {
-            console.log('> Ran into error while canceling message ID', message.id, error);
-        }
-    }
-}
+	for (let message of messageArray) {
+		try {
+			await app.client.chat.deleteScheduledMessage({
+				channel: message[1],
+				scheduled_message_id: message[0],
+				token,
+			});
+		} catch (error) {
+			console.log(
+				"> Ran into error while canceling message ID",
+				message.id,
+				error
+			);
+		}
+	}
+};
 
 (async () => {
-    await app.start(process.env.PORT || 3000);
-    console.log('⚡️ Bolt app is running!');
+	await app.start(process.env.PORT || 3000);
+	console.log("⚡️ Bolt app is running!");
 })();
