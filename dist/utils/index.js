f3648030e6c9f9c1f521609da6c6169590f85c24
Jueun Grace Yun <graceyunn@gmail.com>
2022-05-27 23:31:42 -0400

    git ignore

--- dist/utils/index.js
+++
@@ -1,6 +0,0 @@
-export { default as generateDates } from './generateDates.js';
-export { default as createSchedulerView } from './createSchedulerView.js';
-export { default as createInstallationStore } from './createInstallationStore.js';
-export { default as scheduleMessages } from './scheduleMessages.js';
-export { default as deleteScheduledMessages } from './deleteScheduledMessages.js';
-export { default as createSchedulerResponse } from './createSchedulerResponse.js';
